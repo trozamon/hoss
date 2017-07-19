539a5e0b1f64c93afa3d85a247cd334c0c06256a
Alec Ten Harmsel <alec@alectenharmsel.com>
2017-07-18 20:10:43 -0600

    Send heartbeats from builder to scheduler

--- src/builder/Builder.cpp
+++ src/builder/Builder.cpp
@@ -1,15 +1,20 @@
 #include "Builder.hpp"
 #include "core/Log.hpp"
 #include "core/Socket.hpp"
+#include "msg1/Heartbeat.pb.h"
+#include <array>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 
 using boost::asio::async_connect;
+using boost::asio::deadline_timer;
 using boost::asio::io_service;
 using boost::asio::ip::tcp;
+using boost::system::error_code;
 using hoss::builder::Builder;
 using hoss::core::Log;
 using hoss::core::Socket;
+using std::array;
 
 class Builder::Impl
 {
@@ -20,12 +25,21 @@ public:
 
         int run();
 
-        void connected(const boost::system::error_code &error, 
+        void handleConnect(const error_code &error,
                         tcp::resolver::iterator endpoint);
+
+        void handleHeartbeat(const error_code &error);
+
+        void handleRead(const error_code &error,
+                        std::size_t bytesRead);
+
+        void handleWrite(const error_code &error,
+                        std::size_t bytesWritten);
 
 private:
         io_service svc;
         tcp::resolver resolver;
+        array<char, 4096> buf;
         Log log;
         Socket socket;
 };
@@ -61,7 +75,7 @@ int Builder::Impl::run()
         tcp::resolver::iterator endpoints = resolver.resolve(query);
 
         async_connect(socket.get(), endpoints,
-                        boost::bind(&Builder::Impl::connected, this,
+                        boost::bind(&Builder::Impl::handleConnect, this,
                                 boost::asio::placeholders::error,
                                 boost::asio::placeholders::iterator));
 
@@ -70,8 +84,44 @@ int Builder::Impl::run()
         return 0;
 }
 
-void Builder::Impl::connected(const boost::system::error_code &error, 
+void Builder::Impl::handleConnect(const error_code &error,
                 tcp::resolver::iterator endpoint)
 {
-        log.info("Connected!");
+        log.info("Connected, starting to send heartbeats");
+
+        async_read(socket.get(),
+                        boost::asio::buffer(buf),
+                        boost::bind(&Builder::Impl::handleRead, this,
+                                boost::asio::placeholders::error,
+                                boost::asio::placeholders::bytes_transferred));
+
+        error_code tmp;
+        handleHeartbeat(tmp);
+}
+
+void Builder::Impl::handleHeartbeat(const error_code &error)
+{
+        deadline_timer t(svc);
+
+        hoss::msg1::Heartbeat hb;
+        async_write(socket.get(), boost::asio::buffer(hb.SerializeAsString()),
+                        boost::bind(&Builder::Impl::handleWrite,
+                                this,
+                                boost::asio::placeholders::error,
+                                boost::asio::placeholders::bytes_transferred));
+
+        t.expires_from_now(boost::posix_time::milliseconds(2000));
+        t.async_wait(boost::bind(&Builder::Impl::handleHeartbeat,
+                                this, boost::asio::placeholders::error));
+}
+
+void Builder::Impl::handleRead(const error_code &error, std::size_t bytesRead)
+{
+        log.info("Read something");
+}
+
+void Builder::Impl::handleWrite(const error_code &error,
+                std::size_t bytesWritten)
+{
+        log.info("Sent a heartbeat");
 }
